ad6a953169b8ebaecfff0ba68bddd2c46841165c
shikano <pegtar377512@gmail.com>
2025-02-17 00:18:28 +0900

    feat: ブログ新規投稿ボタンを実装

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
 
 export default async function Header() {
   const session = await auth()
@@ -6,7 +7,9 @@ export default async function Header() {
   return (
     <header className="h-[70px] border-b">
       <div className="container mx-auto h-full flex items-center justify-between px-3">
-        <h1 className="text-[1.5rem] font-bold">🔥 honote</h1>
+        <h1 className="text-[1.5rem] font-bold">
+          <Link href="/">🔥 honote</Link>
+        </h1>
         {!session && <form
           action={async () => {
             "use server"
@@ -22,20 +25,33 @@ export default async function Header() {
           >ログイン</button>
         </form>}
 
-        {session &&<form
-          action={async () => {
-            "use server"
-            await signOut()
-          }}
-        >
-          <button
-            type="submit"
-            className="bg-yellow-300 py-1 px-3 rounded-full font-bold 
+
+        {session &&
+          <div className="flex gap-2 items-center">
+            <button type="button" className="bg-yellow-300 py-1 px-3 rounded-full font-bold 
+             hover:bg-yellow-400 hover:shadow-md 
+             transition-all duration-200 ease-in-out 
+             active:scale-95">
+              <Link href="/blogs/new">
+                +
+              </Link>
+            </button>
+            <form
+              action={async () => {
+                "use server"
+                await signOut()
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-yellow-300 py-1 px-3 rounded-full font-bold 
              hover:bg-yellow-400 hover:shadow-md 
              transition-all duration-200 ease-in-out 
              active:scale-95"
-          >ログアウト</button>
-        </form>}
+              >ログアウト</button>
+            </form>
+          </div>
+        }
       </div>
     </header>
   );
