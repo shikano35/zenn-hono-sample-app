07ed45597adfc6b0800a2d7eb870c9f8864bce55
shikano <pegtar377512@gmail.com>
2025-02-17 00:18:14 +0900

    feat: ブログ詳細ページと新規投稿ページを追加し、ブログ一覧を表示する機能を実装

---
+++ src/app/blogs/new/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth";
+import NewForm from "@/components/newForm";
+
+export default async function Page() {
+
+  const session = await auth()
+
+  if (!session) {
+    return <div>ログインしてください。</div>
+  }
+
+  return (
+    <div>
+      <NewForm />
+    </div>
+  );
+}
